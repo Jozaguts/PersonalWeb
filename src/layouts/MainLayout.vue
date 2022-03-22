3212d98c385f26d08a120f8d0e2bd75d1d76c57e
Sagit Gutierrez <jozaguts@gmail.com>
2022-03-22 01:05:44 -0600

    quasar project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hhh Lpr fff">
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
