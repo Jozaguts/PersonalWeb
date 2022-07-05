be74e2487d605c7d63d2a1f1247d5d1ae73a9cd4
Sagit Gutierrez <jozaguts@gmail.com>
2022-07-05 11:20:44 -0500

    feat: subdivided by components

---
+++ src/components/header/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import BrandComponent from './brand.vue'
+import SocialMediaIcons from './social-media-icons.vue'
+import NavBar from './nav-bar.vue'
+</script>
+<template>
+  <q-header class="text-white q-px-lg q-pt-sm" style="height: 20vh">
+    <q-toolbar>
+      <BrandComponent/>
+      <q-space></q-space>
+      <SocialMediaIcons />
+    </q-toolbar>
+    <nav-bar />
+  </q-header>
+</template>
+<style scoped lang="sass">
+ul
+  list-style: none
+  padding: 0
+.underline-animated
+  text-decoration: none
+  cursor: pointer
+  position: relative
+.underline-animated::before
+  content: ""
+  position: absolute
+  width: 100%
+  height: 4px
+  border-radius: 4px
+  background-color: #40c28a
+  bottom: 0
+  left: 0
+  transform-origin: right
+  transform: scaleX(0)
+  transition: transform .3s ease-in-out
+.underline-animated:hover::before
+  transform-origin: left
+  transform: scaleX(1)
+</style>
