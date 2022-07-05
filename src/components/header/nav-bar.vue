c07bdcce2276d06d4bf0a739859b136564ee7e30
Sagit Gutierrez <jozaguts@gmail.com>
2022-07-05 11:16:13 -0500

    feat: nav-bar component

---
+++ src/components/header/nav-bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="absolute-bottom flex justify-center items-baseline">
+    <ul class="text-center flex ">
+      <li
+        v-for="link in links"
+        :key="link.text"
+        class="q-ml-sm"
+      >
+        <router-link
+          :to="link.to"
+          :class="link.class"
+        >
+          {{link.text}}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+  import { links } from "src/models";
+
+</script>
+
+<style scoped lang="sass">
+ul
+  list-style: none
+  padding: 0
+
+</style>
